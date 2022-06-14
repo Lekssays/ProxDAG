409c725d69ad62df8c557a5b0bea7da046a9cb79
Ahmed Lekssays <ahmed@lekssays.com>
2022-06-14 18:15:16 +0200

    add model updates verification by clients

--- network/simulator/peers/client/main.py
+++ network/simulator/peers/client/main.py
@@ -1,6 +1,8 @@
 import model
 import utils
-
+import torch
+import score_pb2
+import verifier
 
 def main():
     print("Models Updates Verifier")
@@ -8,10 +10,11 @@ def main():
     net = model.Net()
     
     sample = net.state_dict()
+    sample = torch.tensor([1,2,3])
     content_bytes = utils.to_bytes(sample)
 
-    modelID = "some id"
-    parents = ["parent1", "parent2"]
+    modelID = "CNN1"
+    parents = ["HckSwavfZ5gceB58aMCYd6wc9Qd5VE2cRhqujiXBfVRv", "CkmuFbBXLgu11PXySQSmdeyodS13TQj67Zse5M6cuDTh"]
     endpoint = "peer1.org1.example.com"
     model_update_pb = utils.to_protobuf(
         modelID=modelID,
@@ -25,6 +28,35 @@ def main():
 
     messageID = utils.send_model_update(model_update_pb)
     print(messageID)
+
+    model_update = utils.get_model_update(messageID="FYZWEW8wUDzb66E1jp78iLCYFd8KycVjzkiP9hp5VKkG")
+    
+    # Driver for check_trust
+    trust = score_pb2.Trust()
+    trust.scores["pk3"] = 0.223
+    trust.scores["pk1"] = 0.853
+    trust.scores["pk2"] = 0.991
+    trust.scores["pk4"] = 0.441
+    issuers = ["pk4", "pk2"]
+    issuers_trust_scores = verifier.check_trust(trust=trust, issuers=issuers)
+
+    # Driver for check_similarity
+    similarity = score_pb2.Similarity()
+    similarity.n = len(trust.scores)
+    
+
+    for i in range(0, len(trust.scores)):
+        score = score_pb2.Score()
+        for j in range(0, len(trust.scores)):
+            if i == j:
+                score.items.append(0.0)
+            else:
+                score.items.append((i * 10.0) + j)
+        similarity.scores.append(score)
+    
+    print(similarity.scores[2].items[1])
+    sorted_similarities = verifier.check_similarity(similarity=similarity, issuers=issuers_trust_scores)
+    print(sorted_similarities)
 
 if __name__ == '__main__':
     main()
--- network/simulator/peers/client/utils.py
+++ network/simulator/peers/client/utils.py
@@ -1,3 +1,4 @@
+import base64
 import modelUpdate_pb2
 import torch
 import io
@@ -5,6 +6,7 @@ import requests
 
 from collections import OrderedDict
 
+from google.protobuf import text_format
 
 def to_protobuf(modelID: str, parents: list, content: bytes, endpoint: str):
     model_update = modelUpdate_pb2.ModelUpdate()
@@ -30,17 +32,14 @@ def from_bytes(content: bytes):
 
 
 def send_model_update(model_update: modelUpdate_pb2.ModelUpdate):
-    PROXDAG_ENDPOINT = "http://0.0.0.0:8080/chat"
+    PROXDAG_ENDPOINT = "http://0.0.0.0:8080/proxdag"
 
+    data = text_format.MessageToString(model_update)
+
+    print(data)
     payload = {
         'purpose': 17,
-        'data': str(model_update),
-    }
-
-    payload = {
-        'to': 'test',
-        'from': 'tesst',
-        'message': "some id"
+        'data': data,
     }
 
     res = requests.post(PROXDAG_ENDPOINT, json=payload)
@@ -51,4 +50,9 @@ def send_model_update(model_update: modelUpdate_pb2.ModelUpdate):
 
 
 def get_model_update(messageID: str):
-    print("todo")
+    GOSHIMMER_ENDPOINT = "http://0.0.0.0:8081/api/message/"
+    url = GOSHIMMER_ENDPOINT + messageID
+    res = requests.get(url)
+    payload = base64.b64decode(res.json()['payload']['content'])
+    payload = payload[12:]
+    return  text_format.Parse(payload, modelUpdate_pb2.ModelUpdate())
